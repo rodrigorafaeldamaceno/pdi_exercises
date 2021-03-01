76eac570697d8ff7bbd5c9d7b73eccd48618a6a7
rodrigorafaeldamaceno <rodrigorafaeldamaceno@gmail.com>
2021-03-01 14:19:03 -0300

    converter cor

--- primeira_aula/main.cpp
+++ primeira_aula/main.cpp
@@ -1,19 +1,32 @@
 #include <opencv2/opencv.hpp>
+#include <vector>
 
 using namespace cv;
+using namespace std;
 
 int main()
 {
   Mat img, cnv;
-  img = imread("/Users/rodrigo/Pictures/missão.jpg");
+  vector<Mat> canaisCor(3);
+
+  img = imread("/Users/rodrigo/Pictures/missao.jpg");
 
   namedWindow("teste", WINDOW_NORMAL);
   namedWindow("conv", WINDOW_NORMAL);
   // converter img
   cvtColor(img, cnv, COLOR_BGR2Luv);
 
+  // save an image
+  // imwrite("/Users/rodrigo/Pictures/convertidade.jpg", cnv);
+
+  split(cnv, canaisCor);
+
   imshow("teste", img);
   imshow("conv", cnv);
+
+  imshow("canal1", canaisCor[0]);
+  imshow("canal2", canaisCor[1]);
+  imshow("canal3", canaisCor[2]);
 
   waitKey(0);
 
